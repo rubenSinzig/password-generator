f80b0142eb044da59572c5fb57286a98f207e85e
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-13 00:12:28 +0100

    password-generator

--- index.js
+++ index.js
@@ -9,6 +9,20 @@
 // passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
 // passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
 
-const passwordGen = () => {
-    
+const passwordGen = (length) => {
+    // declare a var with all charaters that the password should be includes
+    let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&/+*#/@€()={}[]?";
+    // declare a empty var for the loop
+    let password = ""; 
+    for (let i = 0; i < length; i++) {
+        /* with .charAt() we can pick a character from a string
+           example: 
+           console.log(charaters.charAt(6))
+           will print out the "g" from the string characters in line 14
+        */
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    console.log(password.length);
+    console.log(password);
 };
+passwordGen(30); // change the value inside the () for the length of the password
