5185ce33a3209654ebec7483eb9ab0d55c558fdc
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-12 16:07:37 +0100

    create a password-generator

---
+++ index.js
@@ -0,0 +1,14 @@
+// Extra work :)
+// create a repo for this app. we are coming later to it :)
+// Create a program that generate a random password with the length of 30 character, 
+// every time you call the function should give you a different password
+
+// Examples you will have other passwords for sure
+
+// passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
+// passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
+// passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
+
+const passwordGen = () => {
+    
+};
